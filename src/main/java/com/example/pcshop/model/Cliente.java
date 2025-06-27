b116e8090b796a1ff5ed72e31ad2930c19b30c60
Mattia Calabretta <calabrettamattiapio@gmail.com>
2025-06-27 11:54:27 +0200

    aggiunta del  cliente

---
+++ src/main/java/com/example/pcshop/model/Cliente.java
@@ -0,0 +1,19 @@
+package com.example.pcshop.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonProperty("email")
+    private String email;
+
+}
